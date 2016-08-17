372c666ca477855bd0b47e40e89fc115872fbe82
Kyunghwan Kwon <kwon@toanyone.net>
2016-08-17 23:14:42 +0900

    fs: Fix wrong arguments passing in `mutex_lock_atomic()`
    Changed `mutex_lock_atomic()` to pass the argument as `mutex_t` but it
    was still passing `lock_t`, for that one use `lock_atomic()`.

--- fs/fs.c
+++ fs/fs.c
@@ -32,14 +32,14 @@ void rmfile(struct file *file)
 	list_del(&file->list);
 	spin_unlock_irqrestore(&fdtable_lock, irqflag);
 
-	mutex_lock_atomic(&file->inode->lock.counter);
+	mutex_lock_atomic(&file->inode->lock);
 	if (--file->inode->count <= 0) {
 		list_del(&file->inode->list);
 		kfree(file->inode);
 		/* no need to unlock as it's gone */
 		goto out;
 	}
-	mutex_unlock_atomic(&file->inode->lock.counter);
+	mutex_unlock_atomic(&file->inode->lock);
 
 out:
 	kfree(file->op);
