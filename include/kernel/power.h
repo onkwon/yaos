7a26c9c0e62d44467d9ffe011b4f07ec269bc67b
Kyunghwan Kwon <kwon@toanyone.net>
2019-10-07 05:00:43 +0900

    add sleep_t

--- include/kernel/power.h
+++ include/kernel/power.h
@@ -3,6 +3,19 @@
 
 #include <stdlib.h>
 
+/** Sleep type */
+typedef enum {
+	/** Only the core stopped while all peripherals continue to run */
+	SLEEP_NAP	= 1,
+	/** The core and peripherals stop to run including external clock while
+	 * still retaining the SRAM and register contents */
+	SLEEP_DEEP	= 2,
+	/** RTC and watchdog are the only ones continue to run. All the
+	 * contents of SRAM or the registers are not preserved */
+	SLEEP_BLACKOUT	= 3,
+} sleep_t;
+
 void sys_reboot(size_t msec);
+void enter_sleep_mode(sleep_t sleeptype);
 
 #endif /* __YAOS_POWER_H__ */
